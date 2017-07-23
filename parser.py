2b473ef51fe6849e21f62ae854b8f6d763549774
gdesilva <gusdsw@outlook.com>
2017-07-23 13:23:03 +0100

    Changed code to match the ex49 example
    Added in the example code to build tests against (as per ex. 49 task)

--- parser.py
+++ parser.py
@@ -21,37 +21,81 @@
 # sentence, convert it into a List of Sentence objects to match the input.
 
 
-# class ParserError(Exception):
-#     pass
-#
-# class Sentence(object):
-#
-#     def __init__(self, subject, verb, objects):
-#         self.subject
-#         self.verb
-#         self.objects
+class ParserError(Exception):
+    pass
 
 
-def parser(word_list):
+class Sentence(object):
 
-    for wordtype, word in word_list:
+    def __init__(self, subject, verb, objects):
+        self.subject = subject[1]
+        self.verb = verb[1]
+        self.objects = objects[1]
 
-        # if 'subject' in wordtype:
-        #     drop player, append new subject
 
-        if 'verb' in wordtype:
-            print 'it\'s a verb! the word is %s' % word
 
-        if 'noun' in wordtype:
-            print 'it\'s an object! the word is %s' % word
+def peek(word_list):
+    if word_list:
+        word = word_list[0]
+        return word[0]
+    else:
+        return None
 
-        if 'directions' in wordtype:
-            print 'it\'s a direction! the word is %s' % word
 
-        if 'stop' in wordtype:
-            pass
+def match(word_list, expecting):
+    if word_list:
+        word = word_list.pop(0)
 
-        # number handling
+        if word[0] == expecting:
+            return word
+        else:
+            return none
 
-parser([('verb', 'run'), ('directions', 'north')])
+    else:
+        return None
 
+
+def skip(word_list, word_type):
+    while peek(word_list) == word_type:
+        match(word_list, word_type)
+
+
+def parse_verb(word_list):
+    skip(word_list, 'stop')
+
+    if peek(word_list) == 'verb':
+        return match(word_list, 'verb')
+    else:
+        raise ParserError("Expected a verb next.")
+
+
+def parse_object(word_list):
+    skip(word_list, 'stop')
+    next_word = peek(word_list)
+
+    if next_word == 'noun':
+        return match(word_list, 'noun')
+    elif next_word == 'direction':
+        return match(word_list, 'direction')
+    else:
+        raise ParserError("Expected a noun or direction next.")
+
+
+def parse_subject(word_list):
+    skip(word_list, 'stop')
+    next_word = peek(word_list)
+
+    if next_word == 'noun':
+        return match(word_list, 'noun')
+    elif next_word == 'verb':
+        return ('noun', 'player')
+    else:
+        raise ParserError("Expected a verb next.")
+
+
+def parse_sentence(word_list):
+    subj = parse_subject(word_list)
+    verb = parse_verb(word_list)
+    obj = parse_object(word_list)
+
+    return Sentence(subj, verb, obj)
